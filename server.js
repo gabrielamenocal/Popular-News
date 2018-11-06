cec032c1fd383b6cfb99c9ecae95b00c37c7d3d0
Gabriela Menocal <gmenocal89@gmail.com>
2018-11-05 23:21:05 -0500

    add 2 API to gasp info from

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-// First, tell the console what server.js is doing
 console.log("\n***********************************\n" +
             "Grabbing every thread name and link\n" +
-            "from New YOrk Time's Technology website section:" +
+            "from New York Time's Technology website section:" +
             "\n***********************************\n");
 
-// Making a request via axios for reddit's "webdev" board. We are sure to use old.reddit due to changes in HTML structure for the new reddit. The page's Response is passed as our promise argument.
-axios.get("https://www.nytimes.com/section/technology").then(function(response) {
-
-  // Load the Response into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
+axios.get("https://www.nytimes.com/section/technology").then(function(response) {  
+  var $ = cheerio.load(response.data); 
+  var results = [];  
   $("h2.headline").each(function(i, element) {
-
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
+    var title = $(element).text();   
     var link = $(element).children().attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
     results.push({
       title: title,
       link: link
     });
   });
+  console.log(results);
+});
 
-  // Log the results once you've looped through each of the elements found with cheerio
+console.log("\n***********************************\n" +
+            "Grabbing every thread name and link\n" +
+            "from Google New's Technology website section:" +
+            "\n***********************************\n");
+
+
+axios.get("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pEUVNnQVAB?hl=en-CA&gl=CA&ceid=CA%3Aen").then(function(response) {  
+  var $ = cheerio.load(response.data); 
+  var results = [];  
+  $("a.VDXfz").each(function(i, element) {
+    var title = $(element).text();   
+    var link = $(element).children().attr("href");
+    results.push({
+      title: title,
+      link: link
+    });
+  });
   console.log(results);
 });
