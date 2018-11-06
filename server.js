beb6118ebec59ae50ec57a3200efc6e981ec8753
Gabriela Menocal <gmenocal89@gmail.com>
2018-11-05 23:34:12 -0500

    some changes

--- server.js
+++ server.js
@@ -3,39 +3,35 @@ var axios = require("axios");
 
 console.log("\n***********************************\n" +
             "Grabbing every thread name and link\n" +
-            "from New York Time's Technology website section:" +
+            "from New York Times and Google News's Technology website section:" +
             "\n***********************************\n");
 
 axios.get("https://www.nytimes.com/section/technology").then(function(response) {  
   var $ = cheerio.load(response.data); 
-  var results = [];  
+  var resultsNYtimes = [];  
   $("h2.headline").each(function(i, element) {
     var title = $(element).text();   
     var link = $(element).children().attr("href");
-    results.push({
+    resultsNYtimes.push({
       title: title,
       link: link
     });
   });
-  console.log(results);
+  console.log("New York Times: \n" + resultsNYtimes);
 });
 
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from Google New's Technology website section:" +
-            "\n***********************************\n");
-
+// Google News
 
 axios.get("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pEUVNnQVAB?hl=en-CA&gl=CA&ceid=CA%3Aen").then(function(response) {  
   var $ = cheerio.load(response.data); 
-  var results = [];  
+  var resultsGoogle = [];  
   $("a.VDXfz").each(function(i, element) {
     var title = $(element).text();   
     var link = $(element).children().attr("href");
-    results.push({
+    resultsGoogle.push({
       title: title,
       link: link
     });
   });
-  console.log(results);
+  console.log("Google Results: \n" + resultsGoogle);
 });
