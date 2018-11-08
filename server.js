a2c83f1e28e497127e77e71d8dddb1b037c5728d
Gabriela Menocal <gmenocal89@gmail.com>
2018-11-07 23:45:20 -0500

    with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
+var mongoose = require("mongoose");
+
+var PORT = 8080;
+
+var User = require("/.mongoose.js");
+
+var app = express ();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/userdb", { useNewUrlParser: true })
+
+
+// Route to post our form submission to mongoDB via mongoose
+app.post("/submit", function(req, res) {
+  // Create a new user using req.body
+  User.create(req.body)
+    .then(function(dbUser) {
+      // If saved successfully, send the the new User document to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send the error to the client
+      res.json(err);
+    });
+});
+ 
 
 console.log("\n***********************************\n" +
             "Grabbing every thread name and link\n" +
@@ -18,6 +47,8 @@ axios.get("https://www.nytimes.com/section/technology").then(function(response) 
     });
   });
   console.log("New York Times: \n" + resultsNYtimes);
+  console.log(resultsNYtimes);
+
 });
 
 // Google News
@@ -27,11 +58,22 @@ axios.get("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0
   var resultsGoogle = [];  
   $("a.VDXfz").each(function(i, element) {
     var title = $(element).text();   
-    var link = $(element).children().attr("href");
+    var link = $(element).attr("href");
     resultsGoogle.push({
       title: title,
       link: link
     });
   });
   console.log("Google Results: \n" + resultsGoogle);
+  console.log(resultsGoogle);
+
 });
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+
+
+
+
